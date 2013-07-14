975401c2d69c42c008238fc50618558b7049076a
Sean Dawson <contact@seandawson.info>
2013-07-14 13:09:14 +1000

    Now reads lessons and associated lingQs

--- LingQ2Memrise-cli.py
+++ LingQ2Memrise-cli.py
@@ -8,8 +8,17 @@ from urlparse  import urljoin
 # Constants
 LINGQ_API_URL = "https://www.lingq.com/api/"
 LINGQ_API_PROFILE = "profile"
-LINGQ_API_LESSONS = "{0}/lessons/"
+LINGQ_API_LESSONS = "languages/{0}/lessons"
+LINGQ_API_LESSON_LINGQS = "languages/{0}/lessons/{1}/lingqs/"
 
+# Functions
+def dump(filename, text):
+    f = open(filename, 'w')
+    f.write(text)
+    f.close()
+
+
+# Main
 parser = argparse.ArgumentParser(description='Copy LingQs into Memrise')
 parser.add_argument('lingQApiKeyPath',
                    help='The filename containing the API key for LingQ')
@@ -24,13 +33,21 @@ headers = {'Authorization': 'Token ' + lingQApiKey }
 
 # Access profile
 url = urljoin(LINGQ_API_URL, LINGQ_API_PROFILE)
-response = requests.post(url, headers=headers)
+response = requests.get(url, headers=headers)
 
 if (response.status_code != requests.codes.ok):
     print "Error accessing API:"
     response.raise_for_status()
 
-data = response.json()
+data = None
+
+try:
+    data = response.json()
+except ValueError:
+    print "Could not read json. Dumping response to dump.html"
+    dump("dump.html",response.text.encode('utf8'))
+    exit(1);
+
 language = data["language"]
 
 print "Profile download successful\n"
@@ -47,7 +64,54 @@ choice = raw_input().lower().strip()
 if (choice == "n"):
     sys.exit(0)
     
+print "Loading lessons for language: {0}...".format(language)
+url = urljoin(LINGQ_API_URL, LINGQ_API_LESSONS.format(language))
+print ("Url: {0}".format(url))
 
+response = requests.get(url, headers=headers)
+
+if (response.status_code != requests.codes.ok):
+    print "Error accessing API:"
+    response.raise_for_status()
+
+lessons = None
+try:
+    lessons = response.json()
+except ValueError:
+    print "Could not read json. Dumping response to dump.html"
+    dump("dump.html",response.text.encode('utf8'))
+    exit(1);
+
+
+
+for lesson in lessons:
+    id = lesson["id"]
+    title = lesson["title"]
+    print u"Lesson: {0} (ID: {1})".format(title,id)
+    url = urljoin(LINGQ_API_URL, LINGQ_API_LESSON_LINGQS.format(language, id))
+    
+    print ("URL: {0}".format(url))
+    
+    response = requests.get(url, headers=headers)
+
+    if (response.status_code != requests.codes.ok):
+        print "Error accessing API:"
+        response.raise_for_status()
+
+    lingQs = None
+    try:
+        lingQs = response.json()
+    except ValueError:
+        print "Could not read json. Dumping response to dump.html"
+        dump("dump.html",response.text.encode('utf8'))
+        exit(1);
+    
+    print "{0} linqs were found:".format(len(lingQs))
+        
+    for lingQ in lingQs:
+        print u"\tLingQ: {0}".format(lingQ["term"])
+        
+    print "Done\n"
 
 
 
